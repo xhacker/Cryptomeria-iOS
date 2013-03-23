01d67e22656e22d22d2843361b1f050a4aa6b310
Xhacker Liu <liu.dongyuan@gmail.com>
2013-03-23 18:05:37 +0800

    sample romaji data.

---
+++ Cryptomeria/data/CMChartData.h
@@ -0,0 +1,17 @@
+//
+//  CMChartData.h
+//  Cryptomeria
+//
+//  Created by Xhacker on 2013-03-23.
+//  Copyright (c) 2013 Xhacker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CMChartData : NSObject
+
++ (NSArray *)romaji;
++ (NSArray *)hiragana;
++ (NSArray *)katakana;
+
+@end
---
+++ Cryptomeria/data/CMChartData.m
@@ -0,0 +1,30 @@
+//
+//  CMChartData.m
+//  Cryptomeria
+//
+//  Created by Xhacker on 2013-03-23.
+//  Copyright (c) 2013 Xhacker. All rights reserved.
+//
+
+#import "CMChartData.h"
+
+@implementation CMChartData
+
++ (NSArray *)romaji
+{
+    NSArray *rs = [NSArray arrayWithObjects:
+                   [NSArray arrayWithObjects:@'a', @'i', @'u', @'e', @'o', nil],
+                   [NSArray arrayWithObjects:@'a', @'i', @'u', @'e', @'o', nil],
+                   [NSArray arrayWithObjects:@'a', @'i', @'u', @'e', @'o', nil],
+                   [NSArray arrayWithObjects:@'a', @'i', @'u', @'e', @'o', nil],
+                   [NSArray arrayWithObjects:@'a', @'i', @'u', @'e', @'o', nil],
+                   [NSArray arrayWithObjects:@'a', @'i', @'u', @'e', @'o', nil],
+                   [NSArray arrayWithObjects:@'a', @'i', @'u', @'e', @'o', nil],
+                   [NSArray arrayWithObjects:@'a', @'i', @'u', @'e', @'o', nil],
+                   [NSArray arrayWithObjects:@'a', @'i', @'u', @'e', @'o', nil],
+                   [NSArray arrayWithObjects:@'a', @'i', @'u', @'e', @'o', nil],
+                   nil];
+    return rs;
+}
+
+@end
