b6c776264af004d685378e88e27856419ecd43b7
Xhacker Liu <liu.dongyuan@gmail.com>
2013-03-23 19:02:17 +0800

    finished romaji data.

--- Cryptomeria/data/CMChartData.m
+++ Cryptomeria/data/CMChartData.m
@@ -12,18 +12,34 @@
 
 + (NSArray *)romaji
 {
-    NSArray *rs = [NSArray arrayWithObjects:
-                   [NSArray arrayWithObjects:@"a", @"i", @"u", @"e", @"o", nil],
-                   [NSArray arrayWithObjects:@"a", @"i", @"u", @"e", @"o", nil],
-                   [NSArray arrayWithObjects:@"a", @"i", @"u", @"e", @"o", nil],
-                   [NSArray arrayWithObjects:@"a", @"i", @"u", @"e", @"o", nil],
-                   [NSArray arrayWithObjects:@"a", @"i", @"u", @"e", @"o", nil],
-                   [NSArray arrayWithObjects:@"a", @"i", @"u", @"e", @"o", nil],
-                   [NSArray arrayWithObjects:@"a", @"i", @"u", @"e", @"o", nil],
-                   [NSArray arrayWithObjects:@"a", @"i", @"u", @"e", @"o", nil],
-                   [NSArray arrayWithObjects:@"a", @"i", @"u", @"e", @"o", nil],
-                   [NSArray arrayWithObjects:@"a", @"i", @"u", @"e", @"o", nil],
-                   nil];
+    NSArray *rs = @[
+        @[@"a",   @"i",   @"u",   @"e",   @"o"],
+        @[@"ka",  @"ki",  @"ku",  @"ke",  @"ko"],
+        @[@"sa",  @"shi", @"su",  @"se",  @"so"],
+        @[@"ta",  @"chi", @"tsu", @"te",  @"to"],
+        @[@"na",  @"ni",  @"nu",  @"ne",  @"no"],
+        @[@"ha",  @"hi",  @"fu",  @"he",  @"ho"],
+        @[@"ma",  @"mi",  @"mu",  @"me",  @"mo"],
+        @[@"ya",  @"(i)", @"yu",  @"(e)", @"yo"],
+        @[@"ra",  @"ri",  @"ru",  @"re",  @"ro"],
+        @[@"wa",  @"(i)", @"(u)", @"(e)", @"wo"],
+        @[@"n/m", @"",    @"",    @"",    @""],
+        @[@"ga",  @"gi",  @"gu",  @"ge",  @"go"],
+        @[@"za",  @"ji",  @"zu",  @"ze",  @"zo"],
+        @[@"da",  @"ji",  @"zu",  @"de",  @"do"],
+        @[@"ba",  @"bi",  @"bu",  @"be",  @"bo"],
+        @[@"pa",  @"pi",  @"pu",  @"pe",  @"po"],
+        @[@"kya", @"kyu", @"kyo", @"",    @""],
+        @[@"gya", @"gyu", @"gyo", @"",    @""],
+        @[@"sha", @"shu", @"sho", @"",    @""],
+        @[@"ja",  @"ju",  @"jo",  @"",    @""],
+        @[@"cha", @"chu", @"cho", @"",    @""],
+        @[@"nya", @"nyu", @"nyo", @"",    @""],
+        @[@"hya", @"hyu", @"hyo", @"",    @""],
+        @[@"bya", @"byu", @"byo", @"",    @""],
+        @[@"pya", @"pyu", @"pyo", @"",    @""],
+        @[@"mya", @"myu", @"myo", @"",    @""],
+        ];
     return rs;
 }
 
