e42ad28636e9045cf22d10eb258962145f923e9a
Xhacker Liu <liu.dongyuan@gmail.com>
2013-05-02 16:38:52 +0800

    Webview instead of tableview to display kana chart.

--- Cryptomeria/CMChartViewController.m
+++ Cryptomeria/CMChartViewController.m
@@ -7,16 +7,11 @@
 //
 
 #import "CMChartViewController.h"
-#import "CMChartData.h"
-#import "CMChartCell.h"
 
 @interface CMChartViewController () <UITableViewDataSource>
 
-@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
-@property NSArray *romajiData;
-@property NSArray *hiraganaData;
-@property NSArray *katakanaData;
+@property (strong, nonatomic) IBOutlet UIWebView *webView;
 
 @end
 
@@ -26,50 +21,14 @@
 {
     [super viewDidLoad];
     
-    self.romajiData = [CMChartData romaji];
-    self.hiraganaData = [CMChartData hiragana];
-    self.katakanaData = [CMChartData katakana];
-    
-    self.tableView.dataSource = self;
+    NSString *HTML = [NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"chart" ofType:@"html"]  encoding:NSUTF8StringEncoding error:nil];
+    NSURL *baseURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] resourcePath]];
+    [self.webView loadHTMLString:HTML baseURL:baseURL];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-}
-
-#pragma mark Table view methods
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-	return 1;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return [[CMChartData romaji] count];
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    CMChartCell *cell;
-    NSInteger kanaCount = ((NSArray *)self.hiraganaData[indexPath.row]).count;
-    if (kanaCount == 5) {
-        cell = [tableView dequeueReusableCellWithIdentifier:@"ChartCell"];
-    }
-    else {
-        // youon
-        cell = [tableView dequeueReusableCellWithIdentifier:@"YouonChartCell"];
-    }
-    
-    cell.kanaTitleLabel.text = self.hiraganaData[indexPath.row][0];
-	cell.romajiTitleLabel.text = self.romajiData[indexPath.row][0];
-    for (NSInteger i = 0; i < kanaCount; ++i) {
-        ((UILabel *)cell.kanaLabels[i]).text = [NSString stringWithFormat:@"%@\n%@", self.hiraganaData[indexPath.row][i], self.katakanaData[indexPath.row][i]];
-        ((UILabel *)cell.romajiLabels[i]).text = self.romajiData[indexPath.row][i];
-    }
-    
-    return cell;
 }
 
 @end
